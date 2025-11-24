d204dbb4d273676a3d435d8f43896122868bd70d
GraceW3000 <gracewhitney3000@gmail.com>
2025-11-24 17:06:06 -0600

    Added settings.cs, started making settings screen in unity also.

--- Assets/Scripts/Scenes_Script/MainMenu.cs
+++ Assets/Scripts/Scenes_Script/MainMenu.cs
@@ -1,16 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void NewGameButton()
     {
-        
+        SceneManager.LoadScene("NewGame"); // Load the scene with index 1
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadGameButton()
     {
-        
+        SceneManager.LoadScene("LoadGame"); // Load the scene with index 2
+    }
+
+    // Function to exit the game.
+    public void ExitButton()
+    {
+        Debug.Log("Quit");    // Log quit message to console
+        Application.Quit();   // Quit the application
+    }
+
+    public void SettingsButton()
+    {
+        SceneManager.LoadScene("Settings"); // Load the scene with index 3
+    }
+
+    public void AccomplishmentsButton()
+    {
+        SceneManager.LoadScene("Accomplishments"); // Load the scene with index 4
     }
 }
---
+++ Assets/Scripts/Scenes_Script/Settings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsScreen : MonoBehaviour
+{
+    public void SetVolume (float volume)
+    {
+        // Code to set the game's 
+        Debug.Log(volume);
+    }
+}
