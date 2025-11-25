25212fc831c63a1c6d34939c90968c72316dcb2f
GraceW3000 <gracewhitney3000@gmail.com>
2025-11-25 01:01:59 -0600

    Finaly opened Unity after an hour and a half, got the new scripts in place to code.

--- Assets/Scripts/Scenes_Script/MainMenu.cs
+++ Assets/Scripts/Scenes_Script/MainMenu.cs
@@ -1,1 +1,56 @@
-// enter some code here
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject MainMenuPanel;
+    public GameObject SettingsPanel;
+    public GameObject AccomplishmentsPanel;
+    public GameObject NewGamePanel;
+    public GameObject LoadGamePanel;
+
+    // --- Panel Transitions ---
+
+    public void OpenSettingsPanel()
+    {
+        MainMenuPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+    }
+
+    public void OpenAccomplishmentsPanel()
+    {
+        MainMenuPanel.SetActive(false);
+        AccomplishmentsPanel.SetActive(true);
+    }
+
+    public void OpenNewGamePanel()
+    {
+        MainMenuPanel.SetActive(false);
+        NewGamePanel.SetActive(true);
+    }
+
+    public void OpenLoadGamePanel()
+    {
+        MainMenuPanel.SetActive(false);
+        LoadGamePanel.SetActive(true);
+    }
+
+    // --- SCENE LOADING ---
+    public void StartTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void LoadSavedGame(string savedGameSceneName)
+    {
+        SceneManager.LoadScene(savedGameSceneName); // E.g., SceneManager.LoadScene("Level1")
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/Scenes_Script/UIPanelController.cs
+++ Assets/Scripts/Scenes_Script/UIPanelController.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
 
-public class UIPanelController
+// Attach this script to buttons or panels that handle "Back"
+public class UIPanelController : MonoBehaviour
 {
-    
+    // Assign from Inspector: PanelToClose = this panel, PanelToOpen = MainMenuPanel
+    public GameObject PanelToClose;
+    public GameObject PanelToOpen;
+
+    public void ClosePanel()
+    {
+        PanelToClose.SetActive(false);
+        PanelToOpen.SetActive(true);
+    }
 }
